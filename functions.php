940f573493cba1e5807444ed21658af9e01c6327
Milo Halsey-Perry <milo.halseyperry@gmail.com>
2018-10-08 18:06:21 +0100

    seperated code into functions.php and fixed refresh bug

---
+++ functions.php
@@ -0,0 +1,42 @@
+<?php
+
+function connecttoDB($user, $pass)
+{
+	try {
+		return new PDO('mysql:host=localhost;dbname=people', $user, $pass);  // Try catch statement connects to database into $db var
+		echo'connected';
+	} catch (PDOException $e) {
+		die($e->getMessage());
+	}
+}
+
+function fetchAll($db)
+{
+	$query = $db->prepare('SELECT * FROM names');
+	$query->execute();
+	return $query->fetchAll(PDO::FETCH_OBJ);
+}
+
+function addEntry($db)
+{
+	if ($_POST['first_name']) {
+	$firstName = $_POST['first_name'];
+	}
+
+	if ($_POST['last_name']) {
+	$lastName = $_POST['last_name'];
+	}
+
+	if ($lastName && $firstName) {
+	$query = $db->prepare("INSERT INTO names (first_name, last_name) VALUES (:first_name, :last_name)");
+	$query->execute([
+		'first_name' => $firstName,
+		'last_name' => $lastName,
+	]);
+	header("Location: http://pdotest.test:81/index.php");
+	} else {
+	echo 'fail';
+	return false;
+	}
+}
+?>
