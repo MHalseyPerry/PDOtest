7ebcaa3612e4a1e26b2adf02ed8333dbbee6cf8e
Jake Taylor <me@jaketaylor.co>
2018-10-09 12:43:26 +0100

    Use spaces not tabs, fix indentation

--- create-new.php
+++ create-new.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
-	<head>
+    <head>
 
-	</head>
+    </head>
 
-	<body>
-		<form action="index.php" method="POST">
-			First Name:<br>
-			<input type="text" name="first_name" id="first_name"><br>
-			Last Name:<br>
-			<input type="text" name="last_name" id="last_name"><br>
-			<input type="submit" value="Create This Entry!" />
-		</form>
-	</body>
+    <body>
+        <form action="index.php" method="POST">
+            First Name:<br>
+            <input type="text" name="first_name" id="first_name"><br>
+            Last Name:<br>
+            <input type="text" name="last_name" id="last_name"><br>
+            <input type="submit" value="Create This Entry!" />
+        </form>
+    </body>
 </html>
