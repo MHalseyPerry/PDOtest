5a6f76fd5f9cd650f8def7cf4b67618a0e9cb203
Jake Taylor <me@jaketaylor.co>
2018-10-09 12:43:26 +0100

    Use spaces not tabs, fix indentation

--- index.view.php
+++ index.view.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>NamesDB</title>
-	</head>
+    <head>
+        <title>NamesDB</title>
+    </head>
 
-	<body>
-		<ul>
-			<?php foreach ($names as $names): ?>
-				<li><?= "$names->first_name $names->last_name" ?></li>
-			<?php endforeach; ?>
-		</ul>
+    <body>
+        <ul>
+            <?php foreach ($names as $names): ?>
+                <li><?= "$names->first_name $names->last_name" ?></li>
+            <?php endforeach; ?>
+        </ul>
 
-		<a href="create-new.php">Add Entry!</a>
-	</body>
+        <a href="create-new.php">Add Entry!</a>
+    </body>
 </html>
